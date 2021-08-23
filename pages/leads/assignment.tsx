7932f36875bb6368bbf4ebd19efc99fd3de339ef
Vu Thuy <vuthuycntt1996@gmail.com>
2021-08-23 15:45:46 +0700

    Add lead assignment page

---
+++ pages/leads/assignment.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import AppPage from '../../@crema/hoc/AppPage';
+import PageMeta from '../../@crema/core/PageMeta';
+export default AppPage(() => (
+  <React.Fragment>
+    <PageMeta />
+  </React.Fragment>
+));
